9494348d0e9cdbd42fd7a21edaf47d20d6d414db
asoyanke <asoyanke@gmail.com>
2016-03-27 21:09:39 +0530

    Update 27 March

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # simple_spring_web_service
+
+
+OpenShift uses JBoss EWS  2.0 for Tomcat 7 - simple spring web service sample
+
+Listener URL
+
+http://simplespringwebservice-asoyanke.rhcloud.com/requestlistener
+
+GEt Logger URL
+
+http://simplespringwebservice-asoyanke.rhcloud.com/readlogger
--- src/main/java/org/ashok/simple_spring_web_service/controller/HomeController.java
+++ src/main/java/org/ashok/simple_spring_web_service/controller/HomeController.java
@@ -1,9 +1,17 @@
 package org.ashok.simple_spring_web_service.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Scanner;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.FileAppender;
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,11 +36,72 @@ public class HomeController {
 	@RequestMapping(value="/requestlistener")
 	public ResponseEntity<String> requestlistener(@RequestBody String xmlString){
 		
-		logger.info(xmlString);
+		xmlString = "Request Body : START \n" + xmlString + "\nEND"; 
 		
+		logger.info(xmlString);
 		System.out.println(xmlString);
 		
-		result = "1234";
+		result = xmlString;
 		return new ResponseEntity(result, HttpStatus.ACCEPTED);
 	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@RequestMapping(value="/readlogger")
+	public ResponseEntity<String> readlogger(){
+		
+		String xmlString ="";
+		
+		FileAppender fa = (FileAppender) logger.getRootLogger().getAppender("file");
+		xmlString = fa.getFile();
+		result = readFile(xmlString);
+		return new ResponseEntity(result, HttpStatus.ACCEPTED);
+	}
+	
+/*	private String readFile(String filename)
+	{
+	    String content = null;
+	    File file = new File(filename); //for ex foo.txt
+	    FileReader reader = null;
+	    try {
+	        reader = new FileReader(file);
+	        char[] chars = new char[(int) file.length()];
+	        reader.read(chars);
+	        content = new String(chars);
+	        reader.close();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if(reader !=null){try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}}
+	    }
+	    return content;
+	}*/
+	
+	private String readFile(String pathname) {
+
+	    File file = new File(pathname);
+	    StringBuilder fileContents = new StringBuilder((int)file.length());
+
+	    Scanner scanner = null;
+		try {
+			scanner = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	    
+	    String lineSeparator = "\n";
+
+	    try {
+	        while(scanner.hasNextLine()) {        
+	            fileContents.append(scanner.nextLine() + lineSeparator);
+	        }
+	        return fileContents.toString();
+	    } finally {
+	        scanner.close();
+	    }
+	}
+	
 }
